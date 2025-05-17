5a44081a436d2a6fe51e5d92b80bfe2639e85782
X. Wang <xuyi@tetra.nl>
2025-05-17 17:04:13 +0200

    final version

--- models.js
+++ models.js
@@ -1,159 +1,166 @@
-(function ($, Backbone, _, app) {
+(function($, Backbone, _, app) {
 
-    // CSRF helper functions taken directly from Django docs
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method));
+  // CSRF helper functions taken directly from Django docs
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method));
+  }
+
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = $.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(
+            cookie.substring(name.length + 1));
+          break;
+        }
+      }
     }
+    return cookieValue;
+  }
 
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = $.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(
-                    cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
+  // Setup jQuery ajax calls to handle CSRF
+  $.ajaxPrefilter(function(settings, originalOptions, xhr) {
+    var csrftoken;
+    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+      // Send the token to same-origin, relative URLs only.
+      // Send the token only if the method warrants CSRF protection
+      // Using the CSRFToken value acquired earlier
+      csrftoken = getCookie('csrftoken');
+      xhr.setRequestHeader('X-CSRFToken', csrftoken);
     }
+  });
 
-    // Setup jQuery ajax calls to handle CSRF
-    $.ajaxPrefilter(function (settings, originalOptions, xhr) {
-        var csrftoken;
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            // Send the token to same-origin, relative URLs only.
-            // Send the token only if the method warrants CSRF protection
-            // Using the CSRFToken value acquired earlier
-            csrftoken = getCookie('csrftoken');
-            xhr.setRequestHeader('X-CSRFToken', csrftoken);
-        }
-    });
+  var Session = Backbone.Model.extend({
+    defaults: {
+      token: null
+    },
+    initialize: function(options) {
+      this.options = options;
+      $.ajaxPrefilter($.proxy(this._setupAuth, this));
+      this.load();
+    },
+    load: function() {
+      var token = localStorage.apiToken;
+      if (token) {
+        this.set('token', token);
+      }
+    },
+    save: function(token) {
+      this.set('token', token);
+      if (token === null) {
+        localStorage.removeItem('apiToken');
+      } else {
+        localStorage.apiToken = token;
+      }
+    },
+    delete: function() {
+      this.save(null);
+    },
+    authenticated: function() {
+      return this.get('token') !== null;
+    },
+    _setupAuth: function(settings, originalOptions, xhr) {
+      if (this.authenticated()) {
+        xhr.setRequestHeader(
+          'Authorization',
+          'Token ' + this.get('token')
+        );
+      }
+    }
+  });
 
-    var Session = Backbone.Model.extend({
-            defaults: {
-            token: null
-        },
-        initialize: function (options) {
-            this.options = options;
-            $.ajaxPrefilter($.proxy(this._setupAuth, this));
-            this.load();
-        },
-        load: function () {
-            var token = localStorage.apiToken;
-            if (token) {
-                this.set('token', token);
-            }
-        },
-        save: function (token) {
-            this.set('token', token);
-            if (token === null) {
-                localStorage.removeItem('apiToken');
-            } else {
-                localStorage.apiToken = token;
-            }
-        },
-        delete: function () {
-            this.save(null);
-        },
-        authenticated: function () {
-            return this.get('token') !== null;
-        },
-        _setupAuth: function (settings, originalOptions, xhr) {
-            if (this.authenticated()) {
-                xhr.setRequestHeader(
-                    'Authorization',
-                    'Token ' + this.get('token')
-                );
-            }
-        }
-    });
+  app.session = new Session();
 
-    app.session = new Session();
+  var BaseModel = Backbone.Model.extend({
+    url: function() {
+      var links = this.get('links'),
+        url = links && links.self;
+      if (!url) {
+        url = Backbone.Model.prototype.url.call(this);
+      }
+      return url;
+    }
+  });
 
-    var BaseModel = Backbone.Model.extend({
-        url: function () {
-            var links = this.get('links'),
-                url = links && links.self;
-            if (!url) {
-                url = Backbone.Model.prototype.url.call(this);
-            }
-            return url;
-        }
-    });
+  app.models.Category = BaseModel.extend({
+    fetchItems: function() {
+      var id = this.get('id');
+      var name = this.get('name');
+    }
+  });
 
-    app.models.Category = BaseModel.extend({
-        fetchItems: function () {
-            var id = this.get('id');
-            var name = this.get('name');
-        }
-    });
+  app.models.Item = BaseModel.extend({
+    fetchItems: function() {
+      var id = this.get('id');
+      var title = this.get('title');
+    }
+  });
 
-    app.models.Item = BaseModel.extend({
-        fetchItems: function(){
-          var id = this.get('id');
-          var title = this.get('title');
-        }
-    });
+  app.models.User = BaseModel.extend({
+    idAttributemodel: 'username'
+  });
 
-    app.models.User = BaseModel.extend({
-        idAttributemodel: 'username'
-    });
-
-    var BaseCollection = Backbone.Collection.extend({
-        parse: function (response) {
-            this._next = response.next;
-            this._previous = response.previous;
-            this._count = response.count;
-            return response.results || [];
-        },
-        getOrFetch: function (id) {
-            var result = new $.Deferred(),
-                model = this.get(id);
-            if (!model) {
-                model = this.push({id: id});
-                model.fetch({
-                    success: function (model, response, options) {
-                        result.resolve(model);
-                    },
-                    error: function (model, response, options) {
-                        result.reject(model, response);
-                    }
-                });
-            } else {
-                result.resolve(model);
-            }
-            return result;
-        }
-    });
-
-    app.collections.ready = $.getJSON(app.apiRoot);
-    app.collections.ready.done(function (data) {
-        app.collections.Categories = BaseCollection.extend({
-            model: app.models.Category,
-            url: data.category
+  var BaseCollection = Backbone.Collection.extend({
+    parse: function(response) {
+      this._next = response.next;
+      this._previous = response.previous;
+      this._count = response.count;
+      return response.results || [];
+    },
+    getOrFetch: function(id) {
+      var result = new $.Deferred(),
+        model = this.get(id);
+      if (!model) {
+        model = this.push({
+          id: id
         });
-        app.categories = new app.collections.Categories();
-
-        app.collections.Items = BaseCollection.extend({
-            model: app.models.Item,
-            url: data.item,
-            getBacklog: function () {
-                this.fetch({remove: false, data: {backlog: 'True'}});
-            }
+        model.fetch({
+          success: function(model, response, options) {
+            result.resolve(model);
+          },
+          error: function(model, response, options) {
+            result.reject(model, response);
+          }
         });
-        app.items = new app.collections.Items();
+      } else {
+        result.resolve(model);
+      }
+      return result;
+    }
+  });
 
-        app.collections.Users = BaseCollection.extend({
-            model: app.models.User,
-            url: data.users
-        });
-        app.users = new app.collections.Users();
+  app.collections.ready = $.getJSON(app.apiRoot);
+  app.collections.ready.done(function(data) {
+    app.collections.Categories = BaseCollection.extend({
+      model: app.models.Category,
+      url: data.category
     });
+    app.categories = new app.collections.Categories();
+
+    app.collections.Items = BaseCollection.extend({
+      model: app.models.Item,
+      url: data.item,
+      getBacklog: function() {
+        this.fetch({
+          remove: false,
+          data: {
+            backlog: 'True'
+          }
+        });
+      }
+    });
+    app.items = new app.collections.Items();
+
+    app.collections.Users = BaseCollection.extend({
+      model: app.models.User,
+      url: data.users
+    });
+    app.users = new app.collections.Users();
+  });
 
 })(jQuery, Backbone, _, app);
--- views.js
+++ views.js
@@ -1,181 +1,194 @@
-(function ($, Backbone, _, app) {
+(function($, Backbone, _, app) {
 
-    var TemplateView = Backbone.View.extend({
-        templateName: '',
-        initialize: function () {
-            this.template = _.template($(this.templateName).html());
-        },
-        render: function () {
-            var context = this.getContext(),
-            html = this.template(context);
-            this.$el.html(html);
-        },
-        getContext: function () {
-            return {};
+  var TemplateView = Backbone.View.extend({
+    templateName: '',
+    initialize: function() {
+      this.template = _.template($(this.templateName).html());
+    },
+    render: function() {
+      var context = this.getContext(),
+        html = this.template(context);
+      this.$el.html(html);
+    },
+    getContext: function() {
+      return {};
+    }
+  });
+
+  var FormView = TemplateView.extend({
+    events: {
+      'submit form': 'submit',
+      'click button.cancel': 'done'
+    },
+    errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+    clearErrors: function() {
+      $('.error', this.form).remove();
+    },
+    showErrors: function(errors) {
+      _.map(errors, function(fieldErrors, name) {
+        var field = $(':input[name=' + name + ']', this.form),
+          label = $('label[for=' + field.attr('id') + ']', this.form);
+        if (label.length === 0) {
+          label = $('label', this.form).first();
         }
-    });
 
-    var FormView = TemplateView.extend({
-        events: {
-            'submit form': 'submit',
-            'click button.cancel': 'done'
-        },
-        errorTemplate: _.template('<span class="error"><%- msg %></span>'),
-        clearErrors: function () {
-            $('.error', this.form).remove();
-        },
-        showErrors: function (errors) {
-            _.map(errors, function (fieldErrors, name) {
-                var field = $(':input[name=' + name + ']', this.form),
-                    label = $('label[for=' + field.attr('id') + ']', this.form);
-                if (label.length === 0) {
-                    label = $('label', this.form).first();
-                }
-                function appendError(msg) {
-                    label.before(this.errorTemplate({msg: msg}));
-                }
-                _.map(fieldErrors, appendError, this);
-            }, this);
-        },
-        serializeForm: function (form) {
-            return _.object(_.map(form.serializeArray(), function (item) {
-                // Convert object to tuple of (name, value)
-                return [item.name, item.value];
-            }));
-        },
-        submit: function (event) {
-            event.preventDefault();
-            this.form = $(event.currentTarget);
-            this.clearErrors();
-        },
-        failure: function (xhr, status, error) {
-            var errors = xhr.responseJSON;
-            this.showErrors(errors);
-        },
-        done: function (event) {
-            if (event) {
-                event.preventDefault();
-            }
-            this.trigger('done');
-            this.remove();
-        },
-        modelFailure: function (model, xhr, options) {
-            var errors = xhr.responseJSON;
-            this.showErrors(errors);
+        function appendError(msg) {
+          label.before(this.errorTemplate({
+            msg: msg
+          }));
         }
-    });
+        _.map(fieldErrors, appendError, this);
+      }, this);
+    },
+    serializeForm: function(form) {
+      return _.object(_.map(form.serializeArray(), function(item) {
+        // Convert object to tuple of (name, value)
+        return [item.name, item.value];
+      }));
+    },
+    submit: function(event) {
+      event.preventDefault();
+      this.form = $(event.currentTarget);
+      this.clearErrors();
+    },
+    failure: function(xhr, status, error) {
+      var errors = xhr.responseJSON;
+      this.showErrors(errors);
+    },
+    done: function(event) {
+      if (event) {
+        event.preventDefault();
+      }
+      this.trigger('done');
+      this.remove();
+    },
+    modelFailure: function(model, xhr, options) {
+      var errors = xhr.responseJSON;
+      this.showErrors(errors);
+    }
+  });
 
-    var NewItemView = FormView.extend({
-        templateName: '#new-item-template',
-        className: 'new-item',
-        submit: function (event) {
-            var self = this,
-                attributes = {};
-            FormView.prototype.submit.apply(this, arguments);
-            attributes = this.serializeForm(this.form);
-            app.collections.ready.done(function () {
-                app.items.create(attributes, {
-                    wait: true,
-                    success: $.proxy(self.success, self),
-                    error: $.proxy(self.modelFailure, self)
-                });
-            });
-        },
-        getContext: function () {
-            return {categories: app.categories || null};
-        },
-        success: function (model) {
-            this.done();
-            window.location.hash = '#item/' + model.get('id');
-        }
-    });
+  var NewItemView = FormView.extend({
+    templateName: '#new-item-template',
+    className: 'new-item',
+    submit: function(event) {
+      var self = this,
+        attributes = {};
+      FormView.prototype.submit.apply(this, arguments);
+      attributes = this.serializeForm(this.form);
+      app.collections.ready.done(function() {
+        app.items.create(attributes, {
+          wait: true,
+          success: $.proxy(self.success, self),
+          error: $.proxy(self.modelFailure, self)
+        });
+      });
+    },
+    getContext: function() {
+      return {
+        categories: app.categories || null
+      };
+    },
+    success: function(model) {
+      this.done();
+      window.location.hash = '#item/' + model.get('id');
+    }
+  });
 
-    var HomepageView = TemplateView.extend({
-        templateName: '#home-template',
-        events: {
-            'click button.add': 'renderAddForm'
-        },
-        initialize: function (options) {
-            var self = this;
-            TemplateView.prototype.initialize.apply(this, arguments);
-            app.collections.ready.done(function () {
-                var end = new Date();
-                end.setDate(end.getDate() - 7);
-                end = end.toISOString().replace(/T.*/g, '');
-                app.items.fetch({
-                    data: {end_min: end},
-                    success: $.proxy(self.render, self)
-                });
-            });
-        },
-        getContext: function () {
-            return {items: app.items || null};
-        },
-        renderAddForm: function (event) {
-            var view = new NewItemView(),
-                link = $(event.currentTarget);
-            event.preventDefault();
-            link.before(view.el);
-            link.hide();
-            view.render();
-            view.on('done', function () {
-                link.show();
-            });
-        }
-    });
+  var HomepageView = TemplateView.extend({
+    templateName: '#home-template',
+    events: {
+      'click button.add': 'renderAddForm'
+    },
+    initialize: function(options) {
+      var self = this;
+      TemplateView.prototype.initialize.apply(this, arguments);
+      app.collections.ready.done(function() {
+        var end = new Date();
+        end.setDate(end.getDate() - 7);
+        end = end.toISOString().replace(/T.*/g, '');
+        app.items.fetch({
+          data: {
+            end_min: end
+          },
+          success: $.proxy(self.render, self)
+        });
+      });
+    },
+    getContext: function() {
+      return {
+        items: app.items || null
+      };
+    },
+    renderAddForm: function(event) {
+      var view = new NewItemView(),
+        link = $(event.currentTarget);
+      event.preventDefault();
+      link.before(view.el);
+      link.hide();
+      view.render();
+      view.on('done', function() {
+        link.show();
+      });
+    }
+  });
 
-    var LoginView = FormView.extend({
-        id: 'login',
-        templateName: '#login-template',
-        submit: function (event) {
-            var data = {};
-            FormView.prototype.submit.apply(this, arguments);
-            data = this.serializeForm(this.form);
-            $.post(app.apiLogin, data)
-                .done($.proxy(this.loginSuccess, this))
-                .fail($.proxy(this.failure, this));
-        },
-        loginSuccess: function (data) {
-            app.session.save(data.token);
-            window.location = '/static/index.html';
-        }
-    });
+  var LoginView = FormView.extend({
+    id: 'login',
+    templateName: '#login-template',
+    submit: function(event) {
+      var data = {};
+      FormView.prototype.submit.apply(this, arguments);
+      data = this.serializeForm(this.form);
+      $.post(app.apiLogin, data)
+        .done($.proxy(this.loginSuccess, this))
+        .fail($.proxy(this.failure, this));
+    },
+    loginSuccess: function(data) {
+      app.session.save(data.token);
+      window.location = '/static/index.html';
+    }
+  });
 
-    var HeaderView = TemplateView.extend({
-        tagName: 'header',
-        templateName: '#header-template',
-        events: {
-            'click a.logout': 'logout'
-        },
-        getContext: function () {
-            return {authenticated: app.session.authenticated()};
-        },
-        logout: function (event) {
-            event.preventDefault();
-            app.session.delete();
-            window.location = '/static/index.html';
-        }
-    });
+  var HeaderView = TemplateView.extend({
+    tagName: 'header',
+    templateName: '#header-template',
+    events: {
+      'click a.logout': 'logout'
+    },
+    getContext: function() {
+      return {
+        authenticated: app.session.authenticated()
+      };
+    },
+    logout: function(event) {
+      event.preventDefault();
+      app.session.delete();
+      window.location = '/static/index.html';
+    }
+  });
 
-    var ItemView = TemplateView.extend({
-        templateName: '#item-template',
-        initialize: function (options) {
-            var self = this;
-            TemplateView.prototype.initialize.apply(this, arguments);
-            this.itemId = options.itemId;
-            this.item = null;
-        },
-        getContext: function () {
-            return {item: this.item};
-        },
-        render: function () {
-            TemplateView.prototype.render.apply(this, arguments);
-        },
-    });
+  var ItemView = TemplateView.extend({
+    templateName: '#item-template',
+    initialize: function(options) {
+      var self = this;
+      TemplateView.prototype.initialize.apply(this, arguments);
+      this.itemId = options.itemId;
+      this.item = null;
+    },
+    getContext: function() {
+      return {
+        item: this.item
+      };
+    },
+    render: function() {
+      TemplateView.prototype.render.apply(this, arguments);
+    },
+  });
 
-    app.views.HomepageView = HomepageView;
-    app.views.LoginView = LoginView;
-    app.views.HeaderView = HeaderView;
-    app.views.ItemView = ItemView;
+  app.views.HomepageView = HomepageView;
+  app.views.LoginView = LoginView;
+  app.views.HeaderView = HeaderView;
+  app.views.ItemView = ItemView;
 
 })(jQuery, Backbone, _, app);
